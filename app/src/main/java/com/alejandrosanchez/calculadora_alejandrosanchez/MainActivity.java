bab8be084bffb489fb3f987270c9724ccf4de8a6
Minealex <alejandrosanchezpinto@gmail.com>
2023-10-10 15:21:12 +0200

    Creacion de la aplicacion de la calculadora

---
+++ app/src/main/java/com/alejandrosanchez/calculadora_alejandrosanchez/MainActivity.java
@@ -0,0 +1,14 @@
+package com.alejandrosanchez.calculadora_alejandrosanchez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
