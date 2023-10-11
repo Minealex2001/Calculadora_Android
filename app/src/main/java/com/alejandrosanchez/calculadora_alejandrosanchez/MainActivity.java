10dcaca97bb4abd5f84810ad9f756d8bc08c76eb
Minealex_ <alejandrosanchezpinto@gmail.com>
2023-10-11 14:50:11 +0200

    Solo queda arreglar la interfaz de usuario

--- app/src/main/java/com/alejandrosanchez/calculadora_alejandrosanchez/MainActivity.java
+++ app/src/main/java/com/alejandrosanchez/calculadora_alejandrosanchez/MainActivity.java
@@ -2,173 +2,173 @@ package com.alejandrosanchez.calculadora_alejandrosanchez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<Integer> variable1 = new ArrayList<Integer>();
-    private ArrayList<Integer> variable2 = new ArrayList<Integer>();
-    private int resultado = 0;
+    private String variable1 = "";
+    private String variable2 = "";
     private Boolean sumaclick = false;
     private Boolean restaclick = false;
-    private Button numero1, numero2, numero3, numero4, numero5, numero6, numero7, numero8, numero9, numero0, suma, resta, igual, borrar;
-    private TextView pantalla;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        numero0 = findViewById(R.id.button0);
-        numero1 = findViewById(R.id.button1);
-        numero2 = findViewById(R.id.button2);
-        numero3 = findViewById(R.id.button3);
-        numero4 = findViewById(R.id.button4);
-        numero5 = findViewById(R.id.button5);
-        numero6 = findViewById(R.id.button6);
-        numero7 = findViewById(R.id.button7);
-        numero8 = findViewById(R.id.button8);
-        numero9 = findViewById(R.id.button9);
-        suma = findViewById(R.id.buttonmas);
-        igual = findViewById(R.id.buttonresultado);
-        borrar = findViewById(R.id.buttonborrar);
+        Button numero0 = findViewById(R.id.button0);
+        Button numero1 = findViewById(R.id.button1);
+        Button numero2 = findViewById(R.id.button2);
+        Button numero3 = findViewById(R.id.button3);
+        Button numero4 = findViewById(R.id.button4);
+        Button numero5 = findViewById(R.id.button5);
+        Button numero6 = findViewById(R.id.button6);
+        Button numero7 = findViewById(R.id.button7);
+        Button numero8 = findViewById(R.id.button8);
+        Button numero9 = findViewById(R.id.button9);
+        Button suma = findViewById(R.id.buttonmas);
+        Button resta = findViewById(R.id.buttonresta);
+        Button igual = findViewById(R.id.buttonresultado);
+        Button borrar = findViewById(R.id.buttonborrar);
 
-        pantalla = findViewById(R.id.textView);
+        TextView pantalla = findViewById(R.id.textView);
 
         numero0.setOnClickListener(v -> {
-            if (sumaclick || restaclick) {
-                variable1.add(0);
+            if (!sumaclick && !restaclick) {
+                variable1 +="0";
             } else {
-                variable2.add(0);
+                variable2 += "0";
             }
             pantalla.setText(pantalla.getText() + "0");
         });
 
 
         numero1.setOnClickListener(v -> {
-            if (!sumaclick || !restaclick) {
-                variable1.add(1);
+            if (!sumaclick && !restaclick) {
+                variable1 +="1";
             } else {
-                variable2.add(1);
+                variable2 += "1";
             }
             pantalla.setText(pantalla.getText() + "1");
         });
 
 
         numero2.setOnClickListener(v -> {
-            if (!sumaclick || !restaclick) {
-                variable1.add(2);
+            if (!sumaclick && !restaclick) {
+                variable1 +="2";
             } else {
-                variable2.add(2);
+                variable2 += "2";
             }
             pantalla.setText(pantalla.getText() + "2");
         });
 
 
         numero3.setOnClickListener(v -> {
-            if (!sumaclick || !restaclick) {
-                variable1.add(3);
+            if (!sumaclick && !restaclick) {
+                variable1 +="3";
             } else {
-                variable2.add(3);
+                variable2 += "3";
             }
             pantalla.setText(pantalla.getText() + "3");
         });
 
 
         numero4.setOnClickListener(v -> {
-            if (!sumaclick || !restaclick) {
-                variable1.add(4);
+            if (!sumaclick && !restaclick) {
+                variable1 +="4";
             } else {
-                variable2.add(4);
+                variable2 += "4";
             }
             pantalla.setText(pantalla.getText() + "4");
         });
 
 
         numero5.setOnClickListener(v -> {
-            if (!sumaclick || !restaclick) {
-                variable1.add(5);
+            if (!sumaclick && !restaclick) {
+                variable1 +="5";
             } else {
-                variable2.add(5);
+                variable2 += "5";
             }
             pantalla.setText(pantalla.getText() + "5");
         });
 
 
         numero6.setOnClickListener(v -> {
-            if (!sumaclick || !restaclick) {
-                variable1.add(6);
+            if (!sumaclick && !restaclick) {
+                variable1 +="6";
             } else {
-                variable2.add(6);
+                variable2 += "6";
             }
             pantalla.setText(pantalla.getText() + "6");
         });
 
 
         numero7.setOnClickListener(v -> {
-            if (!sumaclick || !restaclick) {
-                variable1.add(7);
+            if (!sumaclick && !restaclick) {
+                variable1 +="7";
             } else {
-                variable2.add(7);
+                variable2 += "7";
             }
             pantalla.setText(pantalla.getText() + "7");
         });
 
 
         numero8.setOnClickListener(v -> {
-            if (!sumaclick || !restaclick) {
-                variable1.add(8);
+            if (!sumaclick && !restaclick) {
+                variable1 +="8";
             } else {
-                variable2.add(8);
+                variable2 += "8";
             }
             pantalla.setText(pantalla.getText() + "8");
         });
 
 
         numero9.setOnClickListener(v -> {
-            if (!sumaclick || !restaclick) {
-                variable1.add(9);
+            if (!sumaclick && !restaclick) {
+                variable1 +="9";
             } else {
-                variable2.add(9);
+                variable2 += "9";
             }
             pantalla.setText(pantalla.getText() + "9");
         });
 
 
         suma.setOnClickListener(v -> {
-            sumaclick = true;
-            pantalla.setText(pantalla.getText() + "+");
+            if (!sumaclick) {
+                sumaclick = true;
+                pantalla.setText(pantalla.getText() + "+");
+            }
+        });
+
+        resta.setOnClickListener(v -> {
+            if (!restaclick) {
+                restaclick = true;
+                pantalla.setText(pantalla.getText() + "-");
+            }
         });
 
 
         igual.setOnClickListener(v -> {
             if (sumaclick) {
-                //resultado = variable1 + variable2;
-                if (resultado != 0) {
-                    // variable1 = resultado;
-                }
-                pantalla.setText(pantalla.getText() + "=" + resultado);
+                variable1 = String.valueOf(Integer.parseInt(variable1) + Integer.parseInt(variable2));
+                pantalla.setText(pantalla.getText() + "=" + variable1);
                 sumaclick = false;
             } else if (restaclick) {
-                // resultado = variable1 - variable2;
-                if (resultado != 0) {
-                    //  variable1 = resultado;
-                }
-                pantalla.setText(pantalla.getText() + "=" + resultado);
+                variable1 = String.valueOf(Integer.parseInt(variable1) - Integer.parseInt(variable2));
+                pantalla.setText(pantalla.getText() + "=" + variable1);
                 restaclick = false;
             }
         });
 
         borrar.setOnClickListener(v -> {
             pantalla.setText("");
-            variable1.clear();
-            variable2.clear();
-            resultado = 0;
+            variable1 = "";
+            variable2 = "";
         });
 
     }
